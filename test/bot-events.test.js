24689091cce8b2405ce470633891691e73b3a9fe
rubiesonthesky <linneawp@gmail.com>
2015-08-13 21:04:42 +0300

    subtype filtering

--- test/bot-events.test.js
+++ test/bot-events.test.js
@@ -76,14 +76,14 @@ describe('Bot Events', function() {
     Bot.prototype.sendToIRC.should.have.not.have.been.called;
   });
 
-  it('should not send messages to irc if it has a subtype', function() {
+  /*it('should not send messages to irc if it has a subtype', function() {
     var message = {
       type: 'message',
       subtype: 'bot_message'
     };
     this.bot.slack.emit('message', message);
     Bot.prototype.sendToIRC.should.have.not.have.been.called;
-  });
+  });*/
 
   it('should send messages to slack', function() {
     var channel = '#channel';
